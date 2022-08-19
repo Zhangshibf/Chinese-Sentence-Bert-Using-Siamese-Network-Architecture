39a938d7f933df11c91d409f89b40f5d8d8becf3
zhangshibf <zhangshibf@gmail.com>
2022-08-19 22:00:33 +0200

    a

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas
 import torch
 import transformers
-
+#a
 tokenizer = transformers.BertTokenizer.from_pretrained("hfl/chinese-bert-wwm")
 model = transformers.BertModel.from_pretrained("hfl/chinese-bert-wwm")
