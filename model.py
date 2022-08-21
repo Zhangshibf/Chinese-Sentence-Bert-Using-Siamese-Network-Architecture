0a4ddf5a954c74062dfef35a57d44d3ce2757a32
zhangshibf <zhangshibf@gmail.com>
2022-08-21 23:23:44 +0200

    a

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import pandas
 import torch
 import transformers
 import json
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from sklearn.utils import shuffle
 #from sentence_transformers import SentenceTransformer, models, losses
 
 def load_data(data_path):
@@ -24,17 +26,44 @@ def load_data(data_path):
             sentences.append(i["sentence2"])
 
     if len(sentences)==2*len(label):
-        #add a shuffle here
         return sentences, label
     else:
         raise Exception("Check your code.")
 
-def create_dataloader(sentences,label):
+def create_dataloader(data_path,batch_size = 25):
+    sentences,label = load_data(data_path)
+
     tokenizer = transformers.BertTokenizer.from_pretrained("hfl/chinese-bert-wwm")
     tokens = tokenizer.batch_encode_plus(sentences, padding=True, return_token_type_ids=False)
-    x = torch.tensor(tokens['input_ids'])
-    x_mask = torch.tensor(tokens['attention_mask'])
+    sentences = tokens['input_ids']
+    masks = tokens['attention_mask']
+
+    sentence_pairs = list()
+    attention_mask_pairs = list()
+    for i in range(len(sentences)):
+        pair = list()
+        if i%2==1:
+            pair.append(sentences[i])
+            pair.append(sentences[i+1])
+            sentence_pairs.append(pair)
+
+    for i in range(len(sentences)):
+        pair = list()
+        if i%2==1:
+            pair.append(masks[i])
+            pair.append(masks[i+1])
+            attention_mask_pairs.append(pair)
+
+    sentence_pairs, attention_mask_pairs = shuffle(sentence_pairs, attention_mask_pairs)
+
+    x = torch.tensor(sentence_pairs)
+    x_mask = torch.tensor(attention_mask_pairs)
     y = torch.tensor(tokens.tolist())
+    data = TensorDataset(x, x_mask, y)
+    sampler = RandomSampler(data)
+    dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+
+    return dataloader
 
 def train_model():
     pass
@@ -42,11 +71,6 @@ def evaluate_model():
     pass
 def create_model():
     word_model = transformers.BertModel.from_pretrained("hfl/chinese-bert-wwm")
-
-
-    
-
-
 #pooling_model
 #sentence model is word model and pooling model ensembled together
 #dataloader is always the same as in the transformers module
