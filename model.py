d66aafdb9fc00a2aade89733bcb8f57687f23caf
zhangshibf <zhangshibf@gmail.com>
2022-08-29 12:49:24 +0200

    a

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch
 import transformers
 import json
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import dataloader
 #from sentence_transformers import SentenceTransformer, models, losses
 
 
@@ -13,6 +14,7 @@ def evaluate_model():
     pass
 def create_model():
     word_model = transformers.BertModel.from_pretrained("hfl/chinese-bert-wwm")
+    model = transformers.BertModel.from_pretrained("MODEL_NAME")
 #pooling_model
 #sentence model is word model and pooling model ensembled together
 #dataloader is always the same as in the transformers module
--- sentence_transformers/Transformer.py
+++ sentence_transformers/Transformer.py
@@ -1,11 +1,12 @@
 from torch import nn
+import transformers
 from transformers import AutoModel, AutoTokenizer, AutoConfig, T5Config
 import json
 from typing import List, Dict, Optional, Union, Tuple
 import os
 
 
-class Transformer(nn.Module):
+class CBERT(nn.Module):
     """Huggingface AutoModel to generate token embeddings.
     Loads the correct class, e.g. BERT / RoBERTa etc.
 
@@ -17,42 +18,33 @@ class Transformer(nn.Module):
     :param do_lower_case: If true, lowercases the input (independent if the model is cased or not)
     :param tokenizer_name_or_path: Name or path of the tokenizer. When None, then model_name_or_path is used
     """
-    def __init__(self, model_name_or_path: str, max_seq_length: Optional[int] = None,
-                 model_args: Dict = {}, cache_dir: Optional[str] = None,
-                 tokenizer_args: Dict = {}, do_lower_case: bool = False,
-                 tokenizer_name_or_path : str = None):
-        super(Transformer, self).__init__()
-        self.config_keys = ['max_seq_length', 'do_lower_case']
-        self.do_lower_case = do_lower_case
+    def __init__(self, max_seq_length: Optional[int] = None, cache_dir: Optional[str] = None,
+                 tokenizer_name="hfl/chinese-bert-wwm",model_name="hfl/chinese-bert-wwm"):
+        super(CBERT, self).__init__()
+        self.config_keys = ['max_seq_length']
+        self.model = transformers.BertModel.from_pretrained(model_name, cache_dir=cache_dir)
+#        config = AutoConfig.from_pretrained(model_name_or_path, **model_args, cache_dir=cache_dir)
 
-        config = AutoConfig.from_pretrained(model_name_or_path, **model_args, cache_dir=cache_dir)
-        self._load_model(model_name_or_path, config, cache_dir)
-
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path if tokenizer_name_or_path is not None else model_name_or_path, cache_dir=cache_dir, **tokenizer_args)
-
-        #No max_seq_length set. Try to infer from model
-        if max_seq_length is None:
-            if hasattr(self.auto_model, "config") and hasattr(self.auto_model.config, "max_position_embeddings") and hasattr(self.tokenizer, "model_max_length"):
-                max_seq_length = min(self.auto_model.config.max_position_embeddings, self.tokenizer.model_max_length)
-
+        self.tokenizer = transformers.BertModel.from_pretrained(tokenizer_name, cache_dir=cache_dir)
         self.max_seq_length = max_seq_length
 
-        if tokenizer_name_or_path is not None:
-            self.auto_model.config.tokenizer_class = self.tokenizer.__class__.__name__
+#       #No max_seq_length set. Try to infer from model
+#        if max_seq_length is None:
+#            if hasattr(self.auto_model, "config") and hasattr(self.auto_model.config, "max_position_embeddings") and hasattr(self.tokenizer, "model_max_length"):
+#                max_seq_length = min(self.auto_model.config.max_position_embeddings, self.tokenizer.model_max_length)
 
+#    def _load_model(self, model_name_or_path, config, cache_dir):
+#        """Loads the transformer model"""
+#        if isinstance(config, T5Config):
+#            self._load_t5_model(model_name_or_path, config, cache_dir)
+#        else:
+#            self.auto_model = AutoModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
 
-    def _load_model(self, model_name_or_path, config, cache_dir):
-        """Loads the transformer model"""
-        if isinstance(config, T5Config):
-            self._load_t5_model(model_name_or_path, config, cache_dir)
-        else:
-            self.auto_model = AutoModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
-
-    def _load_t5_model(self, model_name_or_path, config, cache_dir):
-        """Loads the encoder model from T5"""
-        from transformers import T5EncoderModel
-        T5EncoderModel._keys_to_ignore_on_load_unexpected = ["decoder.*"]
-        self.auto_model = T5EncoderModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
+#    def _load_t5_model(self, model_name_or_path, config, cache_dir):
+#        """Loads the encoder model from T5"""
+#        from transformers import T5EncoderModel
+#        T5EncoderModel._keys_to_ignore_on_load_unexpected = ["decoder.*"]
+#        self.auto_model = T5EncoderModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
 
     def __repr__(self):
         return "Transformer({}) with Transformer model: {} ".format(self.get_config_dict(), self.auto_model.__class__.__name__)
@@ -68,7 +60,7 @@ class Transformer(nn.Module):
 
         features.update({'token_embeddings': output_tokens, 'attention_mask': features['attention_mask']})
 
-        if self.auto_model.config.output_hidden_states:
+        if self.model.config.output_hidden_states:
             all_layer_idx = 2
             if len(output_states) < 3: #Some models only output last_hidden_states and all_hidden_states
                 all_layer_idx = 1
@@ -79,7 +71,7 @@ class Transformer(nn.Module):
         return features
 
     def get_word_embedding_dimension(self) -> int:
-        return self.auto_model.config.hidden_size
+        return self.model.config.hidden_size
 
     def tokenize(self, texts: Union[List[str], List[Dict], List[Tuple[str, str]]]):
         """
@@ -134,7 +126,7 @@ class Transformer(nn.Module):
 
         with open(sbert_config_path) as fIn:
             config = json.load(fIn)
-        return Transformer(model_name_or_path=input_path, **config)
+        return CBERT(model_name_or_path=input_path, **config)
 
 
 
