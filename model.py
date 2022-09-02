045a90cc37ba96357373132b1a268b79b11be369
zhangshibf <zhangshibf@gmail.com>
2022-09-02 18:03:12 +0200

    a

--- model.py
+++ model.py
@@ -10,7 +10,8 @@ from dataloader import create_dataloader
 def train_model(epoch,dataloader,model,optimizer):
     loss_f = nn.CrossEntropyLoss()
     total_loss = 0
-    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+#    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:1')
     model.to(device)
     correct_pred = 0
     for k in range(epoch):
