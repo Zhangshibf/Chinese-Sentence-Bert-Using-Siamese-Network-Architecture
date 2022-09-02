36e71cbde1a161846e65548abd5f9e37bada9f7f
zhangshibf <zhangshibf@gmail.com>
2022-09-02 17:27:15 +0200

    a‘

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from dataloader import create_dataloader
 
 
 def train_model(epoch,dataloader,model,optimizer):
-    loss_f = nn.CosineEmbeddingLoss
+    loss_f = nn.CrossEntropyLoss()
     total_loss = 0
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
