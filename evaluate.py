09bd6b581dd7418b6561045e9acc9d3c57a6ae03
zhangshibf <zhangshibf@gmail.com>
2022-09-21 17:29:56 +0200

    a

--- evaluate.py
+++ evaluate.py
@@ -22,4 +22,5 @@ if __name__ == "__main__":
     eva_model = model.CSBERT(args.model_name)
     path = args.model_path
     eva_model.load_state_dict(torch.load(path))
+    eva_model = eva_model.to(device)
     model.evaluate_model_cosine_similarity(test_dataloader, model=eva_model, device=device)
