58749ce58d3dd2b2cf219dfd6778e8638290cb10
zhangshibf <zhangshibf@gmail.com>
2022-09-21 17:36:29 +0200

    a

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from torch import optim
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train the model')
     parser.add_argument('--train',help = "path to the pickled train set", required=True)
-    parser.add_argument('--out', help="path to save the trained model. The file name ends with .pt", required=True)
+    parser.add_argument('--out', help="path to save the trained model.", required=True)
     parser.add_argument('--device',help = "cuda number", required=True)
     parser.add_argument('--model_name', help="hfl/chinese-macbert-base for MacBert, default bert-wwm",default="hfl/chinese-bert-wwm")
     args = parser.parse_args()
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     with open(args.train, 'rb') as pickle_file:
         train_dataloader = pickle.load(pickle_file)
 
-    epoch = 5
+    epoch = 10
     csbert_model = model.CSBERT(model_name=args.model_name)
     optimizer = optim.SGD(csbert_model.parameters(), lr=0.001, momentum=0.9)
     device_str = "cuda:"+str(args.device)
